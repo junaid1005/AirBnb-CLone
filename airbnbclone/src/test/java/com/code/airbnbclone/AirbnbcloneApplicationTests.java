384e2704139b452189a340abc5d6ac24b1f18b41
junaid1005 <alamjunaid934@gmail.com>
2026-01-25 18:30:18 +0530

    Basic stucture of the project

---
+++ airbnbclone/src/test/java/com/code/airbnbclone/AirbnbcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.airbnbclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
